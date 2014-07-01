1b56cfb2c05b4929244d8ee9a3731b27ca6dbf56
iamijaz <ijaz.ahmed@msn.com>
2014-07-01 13:51:10 +0100

    Context based DI

--- BabyProject/AnonymousMethods/Play.cs
+++ BabyProject/AnonymousMethods/Play.cs
@@ -5,19 +5,31 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace BabyProject.AnonymousMethods
 {
     [TestFixture]
     public class Play
     {
-        private delegate int Square(int val);
+        private delegate int SquareDelegate(int val);
 
         [Test]
         public void C_Sharp_1()
         {
-            var doSquare = new Square(DoSquareMethod);
+            var doSquare = new SquareDelegate(DoSquareMethod);
             Assert.IsTrue(doSquare(3) == 9);
+        }
+
+        [Test]
+        public void C_Sharp_2()
+        {
+            SquareDelegate doSquareDelegate = delegate(int val)
+            {
+                return val*val;
+            };
+
+            Assert.IsTrue(doSquareDelegate(3) == 9);
         }
 
         private static int DoSquareMethod(int val)
