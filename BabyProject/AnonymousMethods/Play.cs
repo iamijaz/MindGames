4795fa47728a78e763e1510c99c8ddf5f1565d52
iamijaz <ijaz.ahmed@msn.com>
2014-07-01 11:29:46 +0100

    Made some string changes and added F# project

---
+++ BabyProject/AnonymousMethods/Play.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BabyProject.AnonymousMethods
+{
+    [TestFixture]
+    public class Play
+    {
+        private delegate int Square(int val);
+
+        [Test]
+        public void C_Sharp_1()
+        {
+            var doSquare = new Square(DoSquareMethod);
+            Assert.IsTrue(doSquare(3) == 9);
+        }
+
+        private static int DoSquareMethod(int val)
+        {
+            return val*val;
+        }
+    }
+}
