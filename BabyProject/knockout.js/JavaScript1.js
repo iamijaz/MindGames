eeb2e13844c193372bbe7e8da4db6d34ceb53481
iamijaz <ijaz.ahmed@msn.com>
2014-07-09 19:01:37 +0100

    Added the observers types and dependent observer exmaple

--- BabyProject/knockout.js/JavaScript1.js
+++ BabyProject/knockout.js/JavaScript1.js
@@ -5,8 +5,15 @@
          nameVisible:ko.observable(true),
          changeName: function () {
              this.name("steve");
-         }
-     };     
+         },
+         
+     };
+
+     viewModel.displayName=
+     ko.computed(function() {
+         return "is" + this.nameVisible();
+     }, viewModel);
+
      ko.applyBindings(viewModel);  
  }
 );
