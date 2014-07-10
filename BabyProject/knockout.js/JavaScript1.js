a2c0783ad2611f7ad6665b338c533bf79404b17d
iamijaz <ijaz.ahmed@msn.com>
2014-07-10 11:02:04 +0100

    Got the listing,addtion and removal of arrays working

--- BabyProject/knockout.js/JavaScript1.js
+++ BabyProject/knockout.js/JavaScript1.js
@@ -1,19 +1,2 @@
-﻿$(
- function () {
-     var viewModel = {
-         name: ko.observable("bob"),
-         nameVisible:ko.observable(true),
-         changeName: function () {
-             this.name("steve");
-         },
-         
-     };
+﻿
 
-     viewModel.displayName=
-     ko.computed(function() {
-         return "is" + this.nameVisible();
-     }, viewModel);
-
-     ko.applyBindings(viewModel);  
- }
-);
