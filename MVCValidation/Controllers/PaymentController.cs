6da56a1723eda87ea2b6586649bb381053ec0b31
iamijaz <ijaz.ahmed@msn.com>
2014-07-30 11:12:24 +0100

    added mvc validation worksheet

---
+++ MVCValidation/Controllers/PaymentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCValidation.Controllers
+{
+    public class PaymentController : Controller
+    {
+        //
+        // GET: /Payment/
+        /*
+        public ActionResult Index()
+        {
+            var model=new PaymentModel();
+            return View(model);
+        }*/
+
+        public ActionResult Index(PaymentModel model)
+        {
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(PaymentModel payment)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", payment);
+            }
+
+            return RedirectToAction("Index", payment);
+        }
+    }
+}
